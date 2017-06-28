8810ac525847acd37ad6cb903cbe23c4066bb5b2
효랭이 <phw898@naver.com>
2017-06-28 10:33:09 +0900

    모바일 js수정

--- WebContent/resource/mobile/js/function.js
+++ WebContent/resource/mobile/js/function.js
@@ -1,7 +1,7 @@
 $(function(){
 	$("#btn-ok").click(function(){
 		//이미지 완료형으로 바꾸는 쿼리
-		$("#stamp").attr("src","/VSampleCoding/resource/mobile/images/stamp.png");
+		$("#stamp").attr("src","/resource/mobile/images/stamp.png");
 		//db에 해당장소에 아이디와 상태값(ture) 저장
 		var id = sessionStorage.getItem("userId");
 		var place = sessionStorage.getItem("place");
