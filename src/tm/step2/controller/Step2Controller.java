ecc121e9513a0a023652dc2c15bf2707783b8d15
woomin <woomin0426@knou.ac.kr>
2017-06-30 16:38:10 +0900

    step2 controller // import StepDTO->Step1DTO 로 변경// by woomin

--- src/tm/step2/controller/Step2Controller.java
+++ src/tm/step2/controller/Step2Controller.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import tm.step1.dto.StepDTO;
+import tm.step1.dto.Step1DTO;
 
 /**
  * Step2Controller
