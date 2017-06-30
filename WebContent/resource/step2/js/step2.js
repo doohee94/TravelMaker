7f46c975695c557d49729080b663053ce128b42c
kosta-PC\kosta <kht518@naver.com>
2017-06-30 19:06:38 +0900

    step2 js주석 by 김현태

--- WebContent/resource/step2/js/step2.js
+++ WebContent/resource/step2/js/step2.js
@@ -1,11 +1,16 @@
 $( function() {
+	//Ul태그의 Li태그를 이동 가능하게 변경
+	//li태그를 받지만 이동은 불가
     $( "ul.changelist" ).sortable();
+    //li태그를 주고 받기 가능
     $( "ul.dropfalse" ).sortable({
     	connectWith: "ul"
     });
-//     $( "#selectlist" ).disableSelection();
     
+    //변경리스트에서 경유지 제거를 할때
+    // 더블 클릭하면 발동
     $(".ui-sortable-handle").dblclick(function(){
+    	//현재 선택된 li태그를 삭제하고 경유지 ul에 삭제된 li를 다시 추가 해준다
     	if($(this).attr("class") == "sellist ui-sortable-handle"){
     		$(".dropfalse").append("<li class='sellist ui-sortable-handle'>"+$(this).text());
     		$(this).remove();
@@ -14,13 +19,20 @@ $( function() {
     	}
     });
     
+    //여행지 추천
     $(".sellist").click(function(){
+    	//그림파일은 숨긴다
     	$("#con").hide();
+    	//선택된 도시를 가져옴
     	var keyword = $(this).text();
     	var key = "CKOKxPOpAhEnu8gzrMSEwqHCW3zGNCxaFWAOjtXL%2B82aon1CjzdWU8g%2BkknZZRWgd9CnHDJywcDr495yzWoV4g%3D%3D";
+    	//ajax를 통해 얻어온 데이터를 div태그안에 출력한다
 		$.ajax({
+			//ajax Url (한국 관광 공사 API)
 			url : "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?ServiceKey="+key
+			//받는 형식으로는 JSON
 			,dataType:"json"
+			//넘겨주는 데이터
 			,data : {
 				"keyword":keyword,
 				"MobileOS":"ETC",
@@ -29,11 +41,13 @@ $( function() {
 				"numOfRows":"6",
 				"_type":"json"
 			}
+			//성공했을때 값을 얻어와 화면에 출력
 			,success:function(data){
 				$(".mainright").text("");
 				//undefined
 				for (var i = 0; i < 7; i++) {
 					$(".mainright").append("<div class='for2"+[i]+" cours2' style='overflow:hidden;'>");
+					//만약 그림이 없다면 noimage로 변경
 					if(data.response.body.items.item[i].firstimage == null || data.response.body.items.item[i].firstimage=="undefined"){
 						$(".for2"+[i]).append("<img class='hover' src='/resource/step2/noimage.jpg' style='width:100%;height:200px;border:1px solid transparent;transition:1s;'>");
 					}else{
@@ -52,13 +66,17 @@ $( function() {
 		}); //end of ajax
 	});
     
+    //경유지를 추가 후 다시 화면을 불러온다
     $("#changelistbtn").click(function(){
     	var str = "";
+    	//리스트의 값을 불러옴
     	$(".changelist li").each(function(){
     		str += $(this).text();
     		str += " ";
     	});
+    	//form안에 값을 입력
     	$("#changelisthidden").val(str.trim());
+    	//form을 submit
     	$("#frm").attr("action","step2.tm");
     	$("#frm").submit();
     });
