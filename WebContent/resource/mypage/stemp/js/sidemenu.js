99d7a8ad12145b54683011264f54f56af7f96251
효랭이 <phw898@naver.com>
2017-07-03 19:33:25 +0900

    효원 뷰랑 리소스 전체 삭제

--- WebContent/resource/mypage/stemp/js/sidemenu.js
+++
@@ -1,8 +0,0 @@
-			//사이드 메뉴 숨겨놨다 보이게 하는 플러그인 
-			$(document).ready(function() {
-				  $('#simple-menu').sidr({
-				    name: 'sidr-main',
-				    source: '#side_menu_nav'
-				  });
-				});
-			
