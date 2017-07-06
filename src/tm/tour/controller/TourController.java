db94b0b0903bc31a701266befc4385eadc735f66
효랭이 <phw898@naver.com>
2017-07-06 16:49:36 +0900

    tour컨트롤러 주석

--- src/tm/tour/controller/TourController.java
+++ src/tm/tour/controller/TourController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
+/**
  * TourController
  * 여행화면 매핑 처리
  * dir : 폴더 경로
@@ -14,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TourController {
 
 	private String dir = "tour/";
-			
+	/**
+	 * main 
+	 * 컴맨드 요청이 없는 일반 호출 
+	 */	
 	@RequestMapping("/{url}.tm")
 	public String main(@PathVariable String url){
 		System.out.println("여행컨트롤러");
