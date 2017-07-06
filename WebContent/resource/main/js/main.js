8137d7baba940596156db3f3ad4ca007e266cbc3
효랭이 <phw898@naver.com>
2017-07-06 17:04:42 +0900

    메인 필요없는 js파일 삭제및 기능 js파일 생성

---
+++ WebContent/resource/main/js/main.js
@@ -0,0 +1,32 @@
+    	$(function(){
+    		//검색버튼 누르면
+    		$('.btn-search').click(function(){
+    			var keyword = $("#search_bar").val();
+    			//search 유효성 검사
+    			if(keyword==""){
+    				alert("검색어가 입력되지 않았습니다.");
+    				return;
+    			}else{
+    			//검색후 serchview화면 이동
+    			$(location).attr('href',"/searchpage/search.tm?cityName="+keyword);
+    			}
+    		});   		
+    		//해당 일정 버튼을 누르면 해당 일정으로 이동
+    		$('.review').click(function(){
+    			//해당 일정 작성자 아이디 받기
+    			var userId = $(this).next().val();
+    			//해당 일정 번호 받기
+    			var scNum = $(this).next().next().val();
+    			//해당 리뷰 번호 받기
+    			var totalNum = $(this).next().next().next().val();
+    			alert("유저아이디 : ["+userId+"] 일정번호 : ["+scNum+"] 리뷰번호 : ["+totalNum+"]");
+    		});
+    		//리뷰 더보기 버튼을 누르면 리뷰리스트 화면으로 이동
+    		$('#reviewMore').click(function(){
+    			alert("리뷰 더보기");
+    		});
+    		$('#spotMore').click(function(){
+    			alert("여행지 더보기");
+    		});
+    		
+    	});
